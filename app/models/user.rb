928c75d6976f0123c9ec59a84c87c9868165553f
MrFuku <cardbed5035@gmail.com>
2019-03-26 12:38:43 +0900

    roomテーブルに関連ユーザーカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one :own_room, class_name: 'Room', :foreign_key => 'owner_id'
+  has_one :guest_room, class_name: 'Room', :foreign_key => 'guest_id'
 end
