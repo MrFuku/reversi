8198a704bab344067b2cbae91fcc3bfc27b3e7c0
MrFuku <cardbed5035@gmail.com>
2019-04-12 13:26:49 +0900

    本番環境メール設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,10 +71,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://fuku-no-othello.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
